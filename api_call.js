13b856da476693c46ee55328e049d36143ab2105
Ashie-github <86165491+Ashie-github@users.noreply.github.com>
2022-05-30 16:36:40 +0200

    Add files via upload

---
+++ api_call.js
@@ -0,0 +1,55 @@
+function api_call() {
+  const message = document.getElementById("message");
+  var body = {
+    grant_type: "urn:ibm:params:oauth:grant-type:apikey",
+    apikey: "VXrWITZG8dZYnY7ahWdhfjrWRkPELL1mAo0HC1tHWdWI",
+  };
+  var postbody = [];
+  for (var element in body) {
+    var encodedkey = encodeURIComponent(element);
+    var encodedvalue = encodeURIComponent(body[element]);
+    postbody.push(encodedkey + "=" + encodedvalue);
+  }
+  postbody = postbody.join("&");
+
+  var req = new XMLHttpRequest();
+  req.open("POST", "https://iam.cloud.ibm.com/identity/token", false);
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  req.send(postbody);
+  if (req.status == 200) {
+    message.innerHTML = req.responseText;
+  }
+
+  // WARNING: For POST requests, body is set to null by browsers.
+
+  //   var data =
+  //     "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=VXrWITZG8dZYnY7ahWdhfjrWRkPELL1mAo0HC1tHWdWI";
+
+  //   console.log(data);
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.withCredentials = false;
+
+  //   xhr.addEventListener("readystatechange", function () {
+  //     if (this.readyState === 4) {
+  //       console.log(this.responseText);
+  //     }
+  //   });
+
+  //   xhr.open("POST", "https://iam.cloud.ibm.com/identity/token");
+  //   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //   xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+  //   xhr.send(data);
+
+  //   var data =
+  //     "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=VXrWITZG8dZYnY7ahWdhfjrWRkPELL1mAo0HC1tHWdWI";
+
+  //   const res = await fetch("https://iam.cloud.ibm.com/identity/token", {
+  //     method: "POST",
+  //     mode: "cors",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: data,
+  //   });
+  //   const jsonResult = await res;
+  //   console.log(jsonResult);
+}
